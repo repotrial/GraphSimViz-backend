42ff4770951cdd97f2769184f97e81aa5c504f82
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2026-04-13 21:49:43 +0200

    updating file download options

--- graphsimviz_backend/views.py
+++ graphsimviz_backend/views.py
@@ -2,10 +2,8 @@ import json
 import mimetypes
 import os
 import uuid
-from wsgiref.util import FileWrapper
 
-from django.http.response import StreamingHttpResponse
-from django.utils.encoding import smart_str
+from django.http import FileResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from graphsimviz_backend import simqt_evaluator
@@ -21,20 +19,28 @@ def get_scores(request) -> Response:
 
 
 @api_view(['GET'])
-def download_results(request) -> Response:
+def download_results(request):
     return download_file(os.path.join(simqt_evaluator.get_data_dir(), 'source_data.zip'))
 
 @api_view(['GET'])
-def download_data(request) -> Response:
+def download_data(request):
     return download_file(os.path.join(simqt_evaluator.get_data_dir(), 'results.zip'))
 
 
 
-def download_file(file) -> Response:
-    response = StreamingHttpResponse(FileWrapper(open(file, 'rb'), 65536), content_type=mimetypes.guess_type(file)[0])
-    response['Content-Disposition'] = 'attachment; filename=' + smart_str(os.path.split(file)[1])
-    response['Content-Length'] = os.path.getsize(file)
-    return response
+def download_file(file_path):
+    if not os.path.exists(file_path):
+        return Response({"error": f"File {file_path} not found"}, status=404)
+    try:
+        # FileResponse will automatically close the file when the response is finished.
+        response = FileResponse(open(file_path, 'rb'), as_attachment=True)
+        # Manually setting content type if guessable, though FileResponse does this too.
+        content_type, _ = mimetypes.guess_type(file_path)
+        if content_type:
+            response['Content-Type'] = content_type
+        return response
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
 
 
 @api_view(['POST'])
